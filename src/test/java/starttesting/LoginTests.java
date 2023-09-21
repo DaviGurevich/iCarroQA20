88823abafd8ecbcd222779374240a036cbda018a
David Gurevich <dmtrgrvch@gmail.com>
2023-09-21 12:22:59 +0300

    mod

--- src/test/java/starttesting/LoginTests.java
+++ src/test/java/starttesting/LoginTests.java
@@ -1,2 +1,31 @@
-package starttesting;public class LogiTests {
+package starttesting;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void preconditions() {
+
+        driver = new ChromeDriver();
+        driver.navigate().to("https://ilcarro.web.app/search");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void loginPositive(){}
+
+
+
+    @AfterClass
+    public void postConditions() {
+
+
+        driver.quit();
+    }
 }
